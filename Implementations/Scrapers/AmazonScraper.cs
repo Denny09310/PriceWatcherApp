097d08076b6fce7d000ce20cb98a6afcced3f7ab
Koja sig. Dennis <DKOJA@ceia-spa.com>
2023-11-28 17:00:30 +0100

    added culture parsing and price scraping

--- Implementations/Scrapers/AmazonScraper.cs
+++ Implementations/Scrapers/AmazonScraper.cs
@@ -1,11 +1,13 @@
+using System.Globalization;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
 using PriceWatcher.Implementations.Contracts.Scrapers;
 using PriceWatcher.Implementations.Models;
 using PuppeteerSharp;
 
 namespace PriceWatcher.Implementations.Scrapers;
 
-public partial class AmazonScraper : IScraper
+public partial class AmazonScraper(ILogger<AmazonScraper> logger) : IScraper
 {
     public Task GetPriceForItemsAsync(IBrowser browser, IEnumerable<ItemWatcher> watchers)
     {
@@ -13,7 +15,7 @@ public partial class AmazonScraper : IScraper
         return Task.WhenAll(workers);
     }
 
-    private static async Task GetPriceForSingleItemAsync(IBrowser browser, ItemWatcher watcher)
+    private async Task GetPriceForSingleItemAsync(IBrowser browser, ItemWatcher watcher)
     {
         using var page = await browser.NewPageAsync();
 
@@ -22,9 +24,23 @@ public partial class AmazonScraper : IScraper
         foreach (var watcherLink in supportedWatcherLinks)
         {
             await page.GoToAsync(watcherLink.Link);
+
+            var priceElementHandle = await page.WaitForSelectorAsync("span.a-price > span.a-offscreen");
+            var priceInnerText = await priceElementHandle.GetPropertyAsync("innerText");
+            var priceValue = await priceInnerText.JsonValueAsync<string>();
+
+            var culture = await page.EvaluateFunctionAsync<string>("() => document.documentElement.lang");
+            var currentCulture = CultureInfo.GetCultureInfo(culture) ?? CultureInfo.CurrentCulture;
+
+            if (!decimal.TryParse(priceValue, NumberStyles.Currency, currentCulture, out var price))
+            {
+                return;
+            }
+
+            logger.LogInformation("Price for item {Title} found: {Price}{CurrencySymbol}", watcher.Title, price, currentCulture.NumberFormat.CurrencySymbol);
         }
     }
 
-    [GeneratedRegex(@"https?:\/\/(www.)?amazon.*")]
+    [GeneratedRegex(@"https?:\/\/(www.)?amazon.\w+/")]
     private static partial Regex AmazonDomainPattern();
 }
